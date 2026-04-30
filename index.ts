74ac9af8cc58f8c8b4fefffd1e83b411d6626c57
roll <eskarev@gmail.com>
2026-04-30 16:06:09 +0100

    refactor: drop f namespace, export createCommand directly

--- index.ts
+++ index.ts
@@ -1,12 +1,3 @@
-import { createCommand } from "./actions/command/create.ts"
-
-/**
- * The fireargs builder namespace. Use `f.command(config?)` to start defining
- * a command. Mark positional arguments via `config.arguments`.
- */
-export const f = {
-  command: createCommand,
-}
-
+export { createCommand } from "./actions/command/create.ts"
 export type { Command } from "./models/command.ts"
 export type { CommandConfig } from "./models/config.ts"
